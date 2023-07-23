1096ea73a04f5ac1c1148226e42b5676b1c421b5
Yash Vaidya <bobbynaldocr7@gmail.com>
2023-07-23 23:26:32 +0530

    Character Array

---
+++ Arrays/characterArray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    char arr[100] = "apple";
+    int i=0;
+
+    while(arr[i] != '\0')
+    {
+        cout << arr[i] << endl;
+        i++;
+    }
+
+    char A[100];
+    cin >> A;
+
+    cout << A ;
+    return 0;
+}
